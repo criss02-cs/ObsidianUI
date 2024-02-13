d9e544cba0dbe987cd787984429f5297fbdd36f9
Cristiano Aloigi <cristianoaloigi0@gmail.com>
2024-02-13 15:07:15 +0100

    cambiata icona dell'applicazione esempio, e sistemato l'apertura del flyout su Android

--- ObsidianUI.Examples/Views/WelcomePage.xaml.cs
+++ ObsidianUI.Examples/Views/WelcomePage.xaml.cs
@@ -2,13 +2,24 @@ namespace ObsidianUI.Examples.Views;
 
 public partial class WelcomePage
 {
+    private bool _alreadyOpen = false;
 	public WelcomePage()
 	{
 		InitializeComponent();
 	}
 
+
 	private void TryOut_Clicked(object? sender, EventArgs e)
-	{
-		Shell.Current.FlyoutIsPresented = !Shell.Current.FlyoutIsPresented;
-	}
+    {
+        if (Shell.Current.FlyoutIsPresented)
+        {
+            Shell.Current.FlyoutIsPresented = false;
+        }
+        else
+        {
+            Shell.Current.FlyoutBehavior = FlyoutBehavior.Locked;
+            Shell.Current.FlyoutBehavior = FlyoutBehavior.Flyout;
+            Shell.Current.FlyoutIsPresented = true;
+        }
+    }
 }
